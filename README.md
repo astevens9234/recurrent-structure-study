67d3f684e70b9a6af32406cb055b8121c92e11a9
Adam <astevens9234@outlook.com>
2025-08-06 12:35:14 -0700

    added mouse_listener, to generate fresh data

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 ### Recurrent Stucture Study
 - This repository is an academic study of various recurrent models.
 
+##### Considered Research
+<arxiv.org/pdf/2005.00890>
+<pmc.ncbi.nlm.nih.gov/articles/PMC8034498/>
+<github.com/vincentbavitz/bezmouse>
+<github.com/riflosnake/humancursor>
+
+##### Citations:
+- First pass stock data provided by <https://www.kaggle.com/datasets/donutkashvi/mousegan>
---
+++ src/mouse_listener.py
@@ -0,0 +1,64 @@
+"""Script to log mouse activity, future training data."""
+
+import csv
+import sys
+import time
+
+from pynput import mouse
+from datetime import datetime as dt
+
+file = input("Enter filename: ") + ".csv"
+cols = ["ts", "event", "x", "y", "button", "action"]
+last_move_time = 0
+interval = 0.05  # 50ms
+
+
+def write_to_csv(event, x, y, button=None, action=None):
+    with open(file, "a", newline="") as f:
+        writer = csv.DictWriter(f, fieldnames=cols)
+        writer.writerow(
+            {
+                "ts": dt.now().timestamp(),
+                "event": event,
+                "x": x,
+                "y": y,
+                "button": button if button else "",
+                "action": action if action else "",
+            }
+        )
+
+
+def on_move(x, y):
+    global last_move_time
+    ct = time.time()
+    if ct - last_move_time >= interval:
+        write_to_csv("move", x, y)
+
+
+def on_click(x, y, button, pressed):
+    action = "press" if pressed else "released"
+    write_to_csv("click", x, y, str(button), action)
+
+
+def on_scroll(x, y, dx, dy):
+    write_to_csv("scroll", x, y, f"delta({dx},{dy})")  # NOTE: format
+
+
+def main():
+    with open(file, "w", newline="") as f:
+        writer = csv.DictWriter(f, fieldnames=cols)
+        writer.writeheader()
+
+    with mouse.Listener(
+        on_move=on_move, on_click=on_click, on_scroll=on_scroll
+    ) as listener:
+        print("mouse monitoring started. Keyboard interupt to stop.")
+        try:
+            listener.join()
+        except KeyboardInterrupt:
+            print("stopped")
+            sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
